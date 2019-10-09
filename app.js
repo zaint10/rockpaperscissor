7bcf8c7c8e798e3151a9a5c36252068a1efc0e00
zaint10 <zat938@gmail.com>
2019-10-10 02:24:47 +0500

    Added Game()

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const userScore=0;
-const compScore=0;
-const scoreBoard_div=document.querySelector("score-board");
+let userScore=0;
+let compScore=0;
+let draws=0;
+const rock_paper_scissor={"r":"Rock","p":"Paper","s":"Scissors"}
+const scoreBoard_div=document.querySelector(".score-board");
 const userScore_span=document.getElementById("user-score");
 const compScore_span=document.getElementById("comp-score");
-const result_div=document.querySelector("result");
+const userLable_div=document.getElementById("users-lable");
+const compLable_div=document.getElementById("comp-lable");
+const drawLable_div=document.getElementById("draw-lable");
+const draw_div=document.getElementById("draws")
+const result_div=document.querySelector(".result > p");
 const rock_div=document.getElementById("r");
 const paper_div=document.getElementById("p");
 const scissors_div=document.getElementById("s");
-// dd
+
+
 
 function getCompChoice(){
 	const choices=['r','p','s'];
@@ -17,26 +24,94 @@ function getCompChoice(){
 
 
 }
+
+function win(userChoice,compChoice){
+	console.log("USER WINS");
+	const userChoice_div=document.getElementById(userChoice);
+	//Increaing Score and displaying it
+	userScore++;
+	userScore_span.innerHTML=userScore;
+	
+	//Converting choice to its appropirate word.
+	userChoice=rock_paper_scissor[userChoice];compChoice=rock_paper_scissor[compChoice];
+	result_div.innerHTML=`${userChoice} beats ${compChoice}. You Win! 💥`;
+
+	//Adding & Removing Glow
+	userChoice_div.classList.add("green-glow");
+	userLable_div.classList.add("green-glow");
+	setTimeout(() => {
+		userChoice_div.classList.remove("green-glow");
+		userLable_div.classList.remove("green-glow");
+		
+	},400);
+}
+function lose(userChoice,compChoice){
+	console.log("COMP WINS!");
+	const userChoice_div=document.getElementById(userChoice);
+	
+	//Increaing Score and displaying it
+	compScore++;
+	compScore_span.innerHTML=compScore;
+	
+	//Converting choice to its appropirate word.
+	userChoice=rock_paper_scissor[userChoice];compChoice=rock_paper_scissor[compChoice];
+	result_div.innerHTML=`${userChoice} loses to ${compChoice}. You Lose!❌`;
+	
+	//Adding Removing Glow
+	userChoice_div.classList.add("red-glow");
+	compLable_div.classList.add("red-glow");
+	setTimeout(() =>{
+		userChoice_div.classList.remove("red-glow");
+		compLable_div.classList.remove("red-glow");}
+	,400);
+}
+
+function draw(userChoice,compChoice){
+	console.log("DRAW!");
+	
+	//Increaing Draws and displaying it
+	draws++;
+	draw_div.innerHTML=draws;
+
+	//Converting choice to its appropirate word.
+	userChoice=rock_paper_scissor[userChoice];compChoice=rock_paper_scissor[compChoice];
+	result_div.innerHTML=`${userChoice} equals ${compChoice}. Its a Draw.`;
+
+	//Adding Removing Glow
+	drawLable_div.classList.add("purple-glow");
+	setTimeout(() => drawLable_div.classList.remove("purple-glow"),400);
+
+}
+
 function game(userChoice){
-	console.log(userChoice);
-	console.log(getCompChoice());
+	const compChoice=getCompChoice();
+	console.log("User choice ->"+userChoice);
+	console.log("Comp choice ->"+compChoice);
+
+	switch(userChoice+compChoice){
+		case "pr":
+		case "rs":
+		case "sp":
+			win(userChoice,compChoice)
+			break;
+		case "rp":
+		case "sr":
+		case "ps":
+			lose(userChoice,compChoice)
+			break;
+		case "rr":
+		case "pp":
+		case "ss":
+			draw(userChoice,compChoice)
+			break;
+
+	}
 }
 
 function main(){
-	rock_div.addEventListener('click',function(){
-	
-	game("r");
-})
-paper_div.addEventListener('click',function(){
-	
-	game("p");
-
-})
-scissors_div.addEventListener('click',function(){
-	
-	game("s");
-
-})
+	rock_div.addEventListener('click', () => game("r"));
+	paper_div.addEventListener('click',() => game("p"));
+	scissors_div.addEventListener('click',()=> game("s"));
 }
 
 main();
